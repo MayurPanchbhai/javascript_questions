2b36f2becfdfd87ba8363bff39197ae0c264f992
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-04-24 14:45:02 +0530

    question 1

--- 1.js
+++ 1.js
@@ -1,1 +1,87 @@
-console.log("hello")
+// 1. Write a function findLongestWord that takes string input and returns the longest word in the string. If there are multiple Longest word , return the first encountered. 
+
+//* Contraints:-
+//? The input string may contain alphabetic character , digits , spaces ans punctuation .
+//? The input string is non-empty.
+//? The input string my contain multiple words separated by spaces.
+
+//* Notes
+//? If the input string is empty or contains only whitespaces , the function should return false .
+//? The function should ignore leading and trailing whitespaces when determining the longest word.
+
+
+findLongestWord = (srt) =>{
+    if (srt.length === 0){
+        return false;
+    }
+
+    const word = srt.split(" ");
+    // console.log(word)
+    for(let i=0;i<word.length ;i++){
+        for(let j=i+1 ; j<word.length; j++){
+            if(word[i].length<word[j].length){
+                samp=word[i];
+                word[i]=word[j];
+                word[j]=samp;
+            }
+            
+        }
+    }
+    return word[0];
+}
+
+
+console.log(
+    findLongestWord("Watch Tapa Technical javascript course on Youtube ")
+);
+
+
+
+
+// method on youtube
+
+/*
+findLongestWord = (srt) =>{
+    if (srt.trim().length === 0){
+        return false;
+    }
+
+    const words=srt.split(" ");
+    words = words.sort((a,b) => b.length-q.length);
+    // console.log(words);
+    return words[0];
+}
+
+console.log(
+    findLongestWord("Watch Tapa Technical javascript course on Youtube ")
+);
+
+*/
+
+
+
+
+// by using the reduce method
+
+/*
+findLongestWord = (srt) =>{
+    if (srt.trim().length === 0){
+        return false;
+    }
+
+    const words=srt.split(" ");
+    return words.reduce(function(acc , curr ){
+        if(acc.length < curr.length){
+            return curr;
+        }
+        else{
+            return acc;
+        }
+         
+    },"")
+}
+
+console.log(
+    findLongestWord("Watch Tapa Technical javascript course on Youtube ")
+);
+*/
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Here I practice javascript interview questions on the daily basis
+
+Here is the list of all the questions along with the number
+
+1. Write a function findLongestWord that takes string input and returns the longest word in the string. If there are multiple Longest word , return the first encountered. 
+
