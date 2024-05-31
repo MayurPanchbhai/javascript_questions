39d2d63c65d4baba34fefe85272c871a51aaa6fb
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-05-31 21:59:25 +0530

    sort Array

--- 5.js
+++ 5.js
@@ -2,10 +2,17 @@
 //* Interview Question: Arrenge the elements in the array by Ascending order
 //*
 
+const sortAscending = (arr) =>{
+    return arr.sort(function(a,b){
+        return a-b;
+    })
+}
+
+
+
 //Example 
 console.log(sortAscending([5,3,1,8]));//output:[1,2,5,8]
-
-console.log(typeof sortAscending[2]);
+// console.log(sortAscending([5,3,10,8]));//output:[1,2,5,8]
 
 //todo 
 //? The function should take an array number as input.
